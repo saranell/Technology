ea9a689677050b6cc6de2c0a8f654811cb339ae0
saranell <76405224+saranell@users.noreply.github.com>
2022-09-06 20:34:13 -0500

    First commit

---
+++ Technology/Computer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Technology
+{
+	public class Computer
+	{
+		public double Ram { get; set; }
+		public double Storage { get; set; }
+		public string OperatingSystem { get; set; }
+		
+
+		public Computer(double ram, double storage, string operatingSystem)
+		{
+			Ram = ram;
+			Storage = storage;
+			OperatingSystem = operatingSystem;
+		}
+
+		public double IncreaseRam(double extraRam)
+        {
+			return Ram += extraRam;
+        }
+
+		public double IncreaseStorage(double extraStorage)
+        {
+			return Storage += extraStorage;
+        }
+	}
+
+}
---
+++ Technology/Laptop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Technology
+{
+    public class Laptop : Computer
+    {
+        public int SoftwareDate { get; set; }
+        public string Dimensions { get; set; }
+
+        public Laptop(double ram, double storage, string operatingSystem, int softwareDate, string dimensions) : base(ram, storage, operatingSystem)
+        {
+            SoftwareDate = softwareDate;
+            Dimensions = dimensions;
+        }
+
+        public bool NeedsSoftwareUpdate()
+        {
+            if (SoftwareDate < 2017)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
