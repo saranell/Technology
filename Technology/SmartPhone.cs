c2722af23d7fa8fdddac8df90508be02f9f6c6df
saranell <76405224+saranell@users.noreply.github.com>
2022-09-07 16:58:42 -0500

    Writing tests

--- Technology/SmartPhone.cs
+++ Technology/SmartPhone.cs
@@ -1,22 +1,22 @@
-﻿using System;
-namespace Technology
-{
-    public class SmartPhone : Computer
-    {
-        public string Carrier { get; set; }
-        public string[] Applications { get; set; }
+﻿//using System;
+//namespace Technology
+//{
+//    public class SmartPhone : Computer
+//    {
+//        public string Carrier { get; set; }
+//        public List <App> Applications { get; set; }
 
-        public SmartPhone(double ram, double storage, string operatingSystem, int softwareDate, string dimensions, string carrier, string[] applications) : base(ram, storage, operatingSystem)
-        {
-            Carrier = carrier;
-            Applications = applications;
-        }
+//        public SmartPhone(double ram, double storage, string operatingSystem, int softwareDate, string dimensions, string carrier, List <App> application) : base(ram, storage, operatingSystem)
+//        {
+//            Carrier = carrier;
+//            Applications = application;
+//        }
 
-        public string DownloadApplication()
-        {
-            //TODO: if an application is new, add to array
-            if ()
-        }
-    }
-}//here are some changes
+//        public List <App> DownloadApp (App application)
+//        {
+//            Applications.Add(application);
+//            return Applications;
+//        }
+//    }
+//}
 
---
+++ TechnologyTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿namespace TechnologyTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestIncreaseRam()
+    {
+        Computer testComputer = new Computer(2, 3, true);
+        Assert.AreEqual(2, testComputer.Ram);
+        testComputer.IncreaseRam(3);
+        Assert.AreEqual(5, testComputer.Ram);
+    }
+}
