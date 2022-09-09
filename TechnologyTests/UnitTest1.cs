177cceb3038278117f2d1eeac11f7f1ff5c56ccd
saranell <saranell92@gmail.com>
2022-09-09 16:10:43 -0500

    unit tests

--- TechnologyTests/UnitTest1.cs
+++ TechnologyTests/UnitTest1.cs
@@ -4,7 +4,7 @@ namespace TechnologyTests;
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
+    [TestMethod] //Computer test 1 of 3
     public void TestIncreaseRam()
     {
         Computer testComputer = new Computer(2, 3, "Windows");
@@ -13,12 +13,20 @@ public class UnitTest1
         Assert.AreEqual(5, testComputer.Ram);
     }
 
-    [TestMethod]
+    [TestMethod] //Computer test 2 of 3
     public void TestIncreaseStorage()
     {
         Computer testComputer = new Computer(2, 3, "Windows");
         Assert.AreEqual(3, testComputer.Storage);
         testComputer.IncreaseStorage(10);
         Assert.AreEqual(13, testComputer.Storage);
+    }
+
+
+    [TestMethod] //Smartphone test 1 of 3
+    public void TestDownloadApp()
+    {
+        SmartPhone testSmartphone = new SmartPhone(2, 3, "Apple", 2020, "5x3", "AT&T", List < App > application);
+
     }
 }
