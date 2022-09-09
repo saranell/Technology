b7f9350c9101f695d41b8b0447c4d428e2764a88
saranell <76405224+saranell@users.noreply.github.com>
2022-09-09 13:48:17 -0500

    Not sure this will push

--- TechnologyTests/UnitTest1.cs
+++ TechnologyTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿namespace TechnologyTests;
+﻿using Technology;
+namespace TechnologyTests;
 
 [TestClass]
 public class UnitTest1
@@ -6,9 +7,18 @@ public class UnitTest1
     [TestMethod]
     public void TestIncreaseRam()
     {
-        Computer testComputer = new Computer(2, 3, true);
+        Computer testComputer = new Computer(2, 3, "Windows");
         Assert.AreEqual(2, testComputer.Ram);
         testComputer.IncreaseRam(3);
         Assert.AreEqual(5, testComputer.Ram);
+    }
+
+    [TestMethod]
+    public void TestIncreaseStorage()
+    {
+        Computer testComputer = new Computer(2, 3, "Windows");
+        Assert.AreEqual(3, testComputer.Storage);
+        testComputer.IncreaseStorage(10);
+        Assert.AreEqual(13, testComputer.Storage);
     }
 }
